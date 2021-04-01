fbc2be4e90cdada96f1ff2f90d8f6a9ad32d91f9
Kevin Cho <71299755+kccho2254@users.noreply.github.com>
2021-03-31 21:36:54 -0500

    Delete client/node_modules directory

--- client/node_modules/react-alice-carousel/lib/utils/controls.d.ts
+++
@@ -1,7 +0,0 @@
-import { Props, State } from '../types';
-export declare function shouldDisableDots(props: Props, state: State): boolean;
-export declare const getDotsNavigationLength: (itemsCount: number | undefined, itemsInSlide: number | undefined, autoWidth: any) => number;
-export declare const checkIsTheLastDotIndex: (index: number, infinite: boolean, dotsLength: number) => boolean;
-export declare const getItemIndexForDotNavigation: (index: number, isTheLastIndex: boolean, slidesLength: number, itemsInSlide: number) => number;
-export declare const shouldCancelAutoPlayOnAction: (strategy?: string) => boolean;
-export declare const shouldCancelAutoPlayOnHover: (strategy?: string) => boolean;
