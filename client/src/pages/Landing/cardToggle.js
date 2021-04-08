d70063b3243caa29eda70df77d3707caf509ebb1
Katie Cowan <kcowan@twu.edu>
2021-04-07 21:55:02 -0500

    Updated landing page and deleted unnecessary files

--- client/src/pages/Landing/cardToggle.js
+++
@@ -1,6 +0,0 @@
-function classToggle() {
-    var el = document.querySelector('.icon-cards__content');
-    el.classList.toggle('step-animation');
-  }
-  
-  document.querySelector('#toggle-animation').addEventListener('click', classToggle);
