bad3c617b917290aabb64bd43dd10a9c4b501505
Chad Cook <Charles.Cook28@gmail.com>
2021-04-10 08:44:30 -0500

    added complete button and delete button to task

--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -4,11 +4,12 @@ import 'bulma/css/bulma.css';
 import TaskCardModal from "../components/cardModal";
 import { CardsWrap, Card, CardHead, CardBody } from "../components/Cards";
 import $ from "jquery";
-import Table from "../components/TableItem";
+import { IncompleteTable, CompleteTable } from "../components/TableItem";
 // import DeleteBtn from "../components/DeleteBtn";
 import AddBtn from "../components/AddBtn";
 import MonnaieContext from "../utils/MonnaieContext";
 import API from "../utils/API";
+// import IncompleteTable from "../components/TableItem";
 
 // class App extends Component {
 //     state = {
@@ -41,16 +42,6 @@ const sampTasks = [
     },
 ];
 
-const test1 = {
-    description: "test1",
-    dueDate: "05/01/2021"
-}
-
-const test2 = {
-    description: "test2",
-    dueDate: "04/01/2021"
-}
-
 
 const TasksList = () => {
 
@@ -68,48 +59,26 @@ const TasksList = () => {
         .catch(err => console.log(err));
     };
 
+    console.log(globalTasks)
 
     return (
         <>
             <CardsWrap>
                 <Card custStyle="card mr-2 has-background-primary-light">
                     <CardHead>Total # of Tasks</CardHead>
-                    <CardBody>Body1</CardBody>
+                    <CardBody>{globalTasks.length}</CardBody>
                 </Card>
-                <Card custStyle="card ml-2 has-background-info-light">
+                {/* <Card custStyle="card ml-2 has-background-info-light">
                     <CardHead># of Tasks Completed</CardHead>
-                    <CardBody>Body2</CardBody>
-                </Card>
+                    <CardBody>{console.log($("#completed").attr("data-number"))}</CardBody>                    
+                </Card> */}
             </CardsWrap>
             <AddBtn handleClick={showTaskForm}>Add Task</AddBtn>
-            <p>{JSON.stringify(globalTasks)}</p>
-            {/* <div class="modal">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">Task</p>
-                    <button class="delete" aria-label="close" onClick={hideTaskForm}></button>
-                </header>
-                <section class="modal-card-body">
-                    <form onSubmit={handleSubmit}>
-                        <input id="description" class="input" placeholder="Description" />
-                        <input id="date" class="input datepicker" data-provide="datepicker" placeholder="Date Due" />
-                    </form>                    
-                </section>
-                <footer class="modal-card-foot">
-                    <button class="button is-success" onClick={saveTask}>Save</button>
-                    <button class="button" onClick={hideTaskForm}>Cancel</button>
-                </footer>
-            </div>
-        </div> */}
-            <TaskCardModal />
-            {/* <Table tasks={this.state.tasks}></Table>               */}
-            <Table>
+            {/* <p>{JSON.stringify(globalTasks)}</p> */}
 
-            </Table>
-            <Table>
-                
-            </Table>
+            <TaskCardModal />
+            <IncompleteTable />
+            <CompleteTable />
         </>
 
     )
